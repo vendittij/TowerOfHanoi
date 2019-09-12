f539301f11a95ae35fc085abc0a4edaf36c3e2fb
John Venditti <jdv005@bucknell.edu>
2019-09-12 15:33:49 -0400

    Tower with rings and ability to select number of rings

--- tower/src/App.js
+++ tower/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Tower from './comps/TowerOfHanoi.js';
+import BackDrop from './comps/TowerOfHanoi.js';
+import NumberPicker from 'react-widgets/lib/NumberPicker'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Tower/>
+        Tower of Hanoi
+        <BackDrop/>
       </header>
     </div>
   );
