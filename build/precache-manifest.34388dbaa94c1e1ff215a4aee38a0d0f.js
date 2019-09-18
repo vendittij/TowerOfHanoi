6b0c2611ea9c38493cb4ac6eefa932da6ea82b33
John Venditti <jdv005@bucknell.edu>
2019-09-17 23:14:51 -0400

    restructure

---
+++ build/precache-manifest.34388dbaa94c1e1ff215a4aee38a0d0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74c8f49ff4f65b78e9be67be4be813f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b64a418472fed2e69a50",
+    "url": "/static/css/main.31275a0b.chunk.css"
+  },
+  {
+    "revision": "dce86b8debff824a2b8f",
+    "url": "/static/js/2.d9a5178c.chunk.js"
+  },
+  {
+    "revision": "b64a418472fed2e69a50",
+    "url": "/static/js/main.62a8ef9d.chunk.js"
+  },
+  {
+    "revision": "8cdc91b574e3c59c025c",
+    "url": "/static/js/runtime~main.21297842.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
